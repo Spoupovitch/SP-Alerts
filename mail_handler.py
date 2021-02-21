084a52e867d4864ab6729f0d6a20a036396851a6
Spoupovitch <Spoupovitch@gmail.com>
2021-02-21 16:05:15 -0500

    modify constants, subject line.  add attachment to email script, convert pct_chg to floating point

--- mail_handler.py
+++ mail_handler.py
@@ -1,17 +1,30 @@
-import smtplib
-import env
-from email.message import EmailMessage
-import datetime as dt
+import smtplib # Simple Mail Transfer Protocol client session object
+import env # file for environment variables
+from email.message import EmailMessage # allows setting of email message fields
+import datetime as dt # differentiate email messages by timestamp
+
+# Send an email with the JSON list of increasing tickers attached
+
+ATTACHMENT = 'crap.json'
+TIMESTAMP = str(dt.datetime.now().time().strftime("%H:%M:%S"))
 
 # construct email
 msg = EmailMessage()
 msg['From'] = env.EMAIL_SENDER
 msg['To'] = env.EMAIL_RECEIVER
-msg['Subject'] = 'Stock data' + str(dt.datetime.now())
+msg['Subject'] = 'Stock Data Update ' + TIMESTAMP
 msg.set_content('GFY')
 
-# use simple mail transfer protocol class w SSL connection
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(env.EMAIL_SENDER, env.EMAIL_PASSWORD)
+# attach increasing tickers list
+with open(ATTACHMENT, 'r') as inc_tickers:
+    f_data = inc_tickers.read()
+    inc_tickers.close()
 
-    smtp.send_message(msg)
+msg.add_attachment(f_data, subtype='json', filename=TIMESTAMP + '_' + ATTACHMENT)
+# DEBUG
+print(msg)
+
+# use SMTP class w SSL connection
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+#     smtp.login(env.EMAIL_SENDER, env.EMAIL_PASSWORD)
+#     smtp.send_message(msg)
--- stock_data_scraper.py
+++ stock_data_scraper.py
@@ -1,32 +1,27 @@
-# data analysis library
-import pandas as pd
-# stock data retrieval API
-from alpha_vantage.timeseries import TimeSeries
-# set starting and ending dates
-import datetime as dt
-import time
+import pandas as pd # data analysis library
+from alpha_vantage.timeseries import TimeSeries # stock data retrieval API
+import datetime as dt # set starting and ending dates
 import json
-# file for environment variables
-import env
-# needed for file operations
-import os
+import env # file for environment variables
+import os # needed for file operations
 
 # Take in a JSON list of stock ticker symbols
 # Calculate percentage increase for each symbol
 # Compile a JSON list of any increases above the threshold
 
+START_TIME = "9:00AM"
+PCT_CHG_THRESHOLD = 5
+TICKER_LIST = 'company_list.json'
+OUTPUT_FILE = 'crap.json'
+
 if os.path.exists(env.CACHE_PATH):
     # delete python cache folder
     os.remove(env.CACHE_PATH)
-
-    # delete cache's parent folder
-    #os.rmdir(env.CACHE_DIR)
 else:
-    print("Cache does not exist, none deleted")
+    print("No cache found at", env.CACHE_PATH)
 
 # read in a list of company tickers as data frame to check
-ticker_list = pd.read_json('company_list.json')
-#print("Tickers to check:", ticker_list)
+ticker_list = pd.read_json(TICKER_LIST)
 
 # list of tickers and changes to send in email
 alert_list = {}
@@ -40,20 +35,19 @@ for symbol in ticker_list['Symbol']:
     # create time series
     ts = TimeSeries(key=env.ALPHA_VANTAGE_API_KEY, output_format='json')
     data = ts.get_quote_endpoint(symbol)
-    #print(data)
 
     pct_chg = data[0].get('10. change percent')
-    # remove '%' and round percentage change
-    print(symbol, "price change %:", round(float(pct_chg[:-1]), 2))
+    # remove '%', parse float and round percentage change
+    pct_chg = round(float(pct_chg[:-1]), 2)
+    print(symbol, "price change %:", pct_chg)
 
-    if abs(pct_chg > env.PCT_CHG_THRESHOLD):
+    if abs(pct_chg > PCT_CHG_THRESHOLD):
         # DEBUG
         #print(symbol, "price increased above threshold")
 
-        # add symbol and price increase % to alert list
+        # save symbol and price increase %
         alert_list.__setitem__(symbol, pct_chg)
 
-
 # write results to output file
-with open('crap.json', 'w') as output_file:
+with open(OUTPUT_FILE, 'w') as output_file:
     print(json.dumps(alert_list, indent=2), file=output_file)
